42e508e22faabaf1ca498429200c9ad86ffaab9d
GodOnlyKnowsXZY <33745983+GodOnlyKnowsXZY@users.noreply.github.com>
2019-02-03 22:41:37 +0800

    Delete lostserver.sh

--- lostserver.sh
+++
@@ -1,5 +0,0 @@
-wget https://buildlogs.centos.org/c7.1511.00/kernel/20151119220809/3.10.0-327.el7.x86_64/kernel-3.10.0-327.el7.x86_64.rpm
-yum install kernel-3.10.0-327.el7.x86_64.rpm -y
-reboot
-wget --no-check-certificate -qO /tmp/appex.sh "https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh" && bash /tmp/appex.sh 'install'
-#wget --no-check-certificate -qO /tmp/appex.sh "https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh" && bash /tmp/appex.sh 'uninstall'
